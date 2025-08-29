58b0c8dd0cce4d998f1e13c4010da3609dfdc8d6
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-08-29 15:51:20 +0530

    Time: 0 ms (100%), Space: 18.8 MB (23.41%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(n, 0), col(m, 0);
+
+        // Mark rows and columns that contain zero
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Set cells to zero if in a marked row or column
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
