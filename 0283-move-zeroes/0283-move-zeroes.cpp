b0ca23a01d204deb94bbe079dd183a955b7b3c2f
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2026-06-30 00:04:45 +0530

    Time: 0 ms (100%), Space: 25.1 MB (5.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp;
+        for(int num:nums)
+{
+    if(num!=0)temp.push_back(num);
+}    
+while(temp.size()<nums.size()){
+    temp.push_back(0);
+}
+nums=temp;
+    }
+};
